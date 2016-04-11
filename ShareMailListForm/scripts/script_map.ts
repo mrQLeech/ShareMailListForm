a2fea0e3b61f1f83e40c66b499a8e90b29c17d91
QLeech <c-yleech@rambler.ru>
2016-04-12 00:35:19 +0500

    night fight

---
+++ ShareMailListForm/scripts/script_map.ts
@@ -0,0 +1,4 @@
+/// <reference path='../libs/declaration/jquery/jquery.d.ts' />
+/// <reference path='../libs/declaration/angular/angular.d.ts' />
+/// <reference path='script_registrant.ts' />
+//# sourceMappingURL=script_map.js.map
