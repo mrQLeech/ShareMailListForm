1d239095e6f128b361f54fd078b93d5c56395d28
QLeech <c-yleech@rambler.ru>
2016-04-12 10:14:43 +0500

    added GglDsc html for example

---
+++ ShareMailListForm/scripts/script_registrant.js
@@ -0,0 +1,6 @@
+/// <reference path='script_map.ts' />
+var testAppModule;
+(function (testAppModule) {
+    'use strict';
+    var testApp = angular.module('testapp', []);
+})(testAppModule || (testAppModule = {}));
---
+++ ShareMailListForm/scripts/script_registrant.ts
@@ -0,0 +1,8 @@
+/// <reference path='script_map.ts' />
+
+
+module testAppModule{
+    'use strict';
+
+    var testApp = angular.module('testapp', []);
+}
